8104fdb76d466312c98fe5c6ed22b360f2e3eb1a
rafalagunas <guitronleafar@gmail.com>
2023-08-09 16:59:19 -0600

    add information route for profile update functionality

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -10,25 +10,40 @@ const router = express.Router();
 const users = [];
 
 router.post("/create", async (req, res) => {
-  const user = req.body.name;
+  const user = req.body.username;
   const hashedPassword = await hashPassword(req.body.password);
-  users.push({ user: user, password: hashedPassword });
+  users.push({ username: user, password: hashedPassword });
   res.status(201).send(users);
   console.log(users);
 });
 
 router.post("/login", async (req, res) => {
-  const user = users.find((x) => x.user == req.body.name);
-
+  const user = users.find((x) => x.username == req.body.username);
   if (user == null) res.status(404).send("User doesn't exists");
-
   if ((await comparePassword(req.body.password, user.password)) === true) {
-    const accessToken = generateAccessToken({ user: req.body.name });
-    const refreshToken = generateRefreshToken({ user: req.body.name });
+    const accessToken = generateAccessToken({ user: req.body.username });
+    const refreshToken = generateRefreshToken({ user: req.body.username });
     res.json({ accessToken: accessToken, refreshToken: refreshToken });
   } else {
     res.status(401).send("Check your Password");
   }
+});
+
+router.post("/add_information", validateToken, async (req, res) => {
+  const user = users.find((x) => x.username == req.body.username);
+  if (user == null) res.status(404).send("User doesn't exists");
+
+  console.log("USER", user);
+  const index = users.findIndex((obj) => {
+    return obj.username === user.username;
+  });
+
+  users[index].lastname = req.body.lastname;
+  users[index].email = req.body.email;
+  users[index].address = req.body.address;
+  users[index].phone = req.body.phone;
+
+  res.status(201).send({ data: { users }, message: "Information updated" });
 });
 
 router.get("/validate_token", validateToken, async (req, res) => {
